b94cd098137fb4c52a3dce321b16a15e58faab34
vivek123456-ai <vivekring786@gmail.com>
2025-01-02 17:36:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3)  //we are using nodejs not browser
+
+console.log(3 + 3);
+console.log("vivek");
+
+
+let name = "vivek"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// data types
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined => 
+// symbol => unique
+
+
+// object =>
+
+
+console.log(typeof state);
+    
